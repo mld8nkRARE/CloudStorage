a1c4650ecb01b58a665a9003a3d5d525fcbdb6c2
Варвара Чернятьева <nikola2485alena@gmail.com>
2025-11-24 23:23:19 +0300

    Добавлены DTO для авторизации и файлов, созданы интерфейсы сервисов

---
+++ Client/Services/Interfaces/IFileService.cs
@@ -0,0 +1,17 @@
+﻿using Client.Models.File;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Services.Interfaces
+{
+    public interface IFileService
+    {
+        Task<List<FileDto>> GetFilesAsync();
+        Task<bool> UploadFileAsync(string filePath);
+        Task<bool> DownloadFileAsync(Guid id, string savePath);
+        Task<bool> DeleteFileAsync(Guid id);
+    }
+}
